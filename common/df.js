1bea93b0dc39ca3c4d111c42f03926eff6a07d3d
inu1255 <929909260@qq.com>
2018-10-29 17:06:28 +0800

    first commit

--- common/df.js
+++ common/df.js
@@ -36,98 +36,6 @@ df.table('verify', [
     df.primary('id')
 ]);
 
-// 团队表
-df.table('team', [
-    df.unsigned('id').notNull().auto_increment(),
-    df.unsigned('create_id').comment('创建者'),
-    df.varchar('name', 16).notNull().comment('团队名'),
-    df.varchar('jobs', 2048).notNull().comment('岗位清单，默认:"管理,产品,开发,测试,内容,运维,客服,销售"'),
-    df.varchar('q', 128).comment('问题,加入团队需要回答'),
-    df.varchar('a', 32).comment('答案,加入团队问题的答案'),
-    df.int('audit').default(0).comment('是否需要审核'),
-    df.int('cnt').default(1).comment('团队人数'),
-    df.primary('id'),
-    df.unique('name'),
-    df.foreign('create_id').references('user', 'id')
-]);
-// 更新 团队人数
-// update team set cnt=1+(select count(*) from team_user where team_id=id and state=3)
-
-// 团队用户表
-df.table('team_user', [
-    df.unsigned('team_id').notNull().comment('团队ID'),
-    df.unsigned('user_id').notNull().comment('用户ID'),
-    df.varchar('job', 16).comment('职位'),
-    df.int('pow').notNull().default(3).comment('权限：0x02-可查看 0x20-可修改'),
-    df.int('state').notNull().comment('状态: 0:黑名单 1:申请中 2:拒绝加入 2:正式成员'),
-    df.text('ext').comment('{a:申请问题答案}'),
-    df.bigint('create_at').notNull().comment('创建时间'),
-    df.primary('team_id,user_id'),
-    df.foreign('team_id').references('team', 'id'),
-    df.foreign('user_id').references('user', 'id')
-]);
-
-// 节点
-df.table('node', [
-    df.unsigned('id').notNull().auto_increment(),
-    df.unsigned('create_id').comment('创建者'),
-    df.unsigned('parent_id').comment('父结点'),
-    df.unsigned('edit_id').comment('最近修改人'),
-    df.varchar('path', 128).notNull().default('/').comment('路径(不含自身)：/1/2/34/'),
-    df.int('state').notNull().default(3).comment('状态:0x01-可写 0x02客人可见'),
-    df.varchar('title', 16).notNull().comment('标题'),
-    df.int('taskSum').notNull().default(0).comment('本节点子任务总数'),
-    df.int('taskOk').notNull().default(0).comment('本节点已完成任务数'),
-    df.int('percent').notNull().default(0).comment('本节点完成度[0,100]'),
-    df.bigint('create_at').notNull().comment('创建时间'),
-    df.bigint('update_at').notNull().default(0).comment('更新时间（节点直属帖子变化(发布、修改、删除、回复、主题移入、主题移出)，初始同创建时间）'),
-    df.bigint('subChg_at').notNull().default(0).comment('子树最近修改时间（子树节点中nTChg最大的一个,0表示没有子节点)'),
-    df.bigint('subDel_at').notNull().default(0).comment('最近删除/移走后代节点的时间'),
-    df.bigint('plan_at').notNull().default(0).comment('计划完成时间'),
-    df.bigint('minPlan_at').notNull().default(0).comment('子树中未完成的主题中的计划时间最早的一个'),
-    df.primary('id'),
-    df.unique('title'),
-    df.index('path'),
-    df.foreign('parent_id').references('node', 'id'),
-    df.foreign('create_id').references('user', 'id')
-]);
-
-// 节点用户关联
-df.table('node_user', [
-    df.unsigned('node_id').notNull().comment('节点ID'),
-    df.unsigned('user_id').notNull().comment('用户ID'),
-    df.bigint('visit_at').notNull().default(0).comment('最近打开时间（此用户获取了节点帖子）'),
-    df.int('pow').comment('权限：0x01-可见 0x02-查看 0x04-下载 0x08-回复 0x10-发布 0x20-修改 0x40-管理 0x100=删除 null-继承父结点权限'),
-    df.varchar('loc', 138).notNull().comment('定位路径(包含自身node_id)：/1/2/34/,为了复杂的检索'),
-    df.primary('node_id, user_id'),
-    df.foreign('node_id').references('node', 'id'),
-    df.foreign('user_id').references('user', 'id')
-]);
-
-// 帖子
-df.table('post', [
-    df.unsigned('id').notNull().auto_increment(),
-    df.unsigned('create_id').comment('创建者'),
-    df.int('type').notNull().default(10).comment('类别(10=闲聊 20=资料 100=其他非代码类任务 150=定义 160=设计 210=改进 220=新功能 230=测试 240=bug)'),
-    df.int('top').notNull().default(0).comment('置顶序号'),
-    df.unsigned('node_id').notNull().comment('关联节点ID'),
-    df.varchar('title', 48).notNull().comment('标题'),
-    df.bigint('create_at').notNull().comment('创建时间'),
-    df.bigint('update_at').notNull().comment('最近修改时间'),
-    df.unsigned('edit_id').notNull().comment('最近修改人id(任何修改，包括移动)'),
-    df.bigint('reply_at').notNull().default(0).comment('最近被回复时间'),
-    df.unsigned('head_id').notNull().default(0).comment('负责人ID,0表示不是任务，无需负责人'),
-    df.bigint('plan_at').notNull().default(0).comment('计划完成时间,创建人填写，负责人改写'),
-    df.int('percent').notNull().default(0).comment('进度,[0,100]，负责人改写'),
-    df.int('attCnt').notNull().default(0).comment('附件数量'),
-    df.text('content').charset('utf8mb4').comment('帖子内容'),
-    df.primary('id'),
-    df.index('node_id'),
-    df.foreign('node_id').references('node', 'id'),
-    df.foreign('create_id').references('user', 'id'),
-    df.foreign('edit_id').references('user', 'id')
-]);
-
 // 文件表
 df.table('file', [
     df.unsigned('id').notNull().auto_increment(),
@@ -139,35 +47,6 @@ df.table('file', [
     df.primary('id'),
     df.unique('md5'),
     df.foreign('create_id').references('user', 'id')
-]);
-
-// 主题－用户访问
-df.table('post_user', [
-    df.unsigned('post_id').notNull().comment('帖子ID'),
-    df.unsigned('user_id').notNull().comment('用户ID'),
-    df.bigint('open_at').notNull().default(0).comment('最近打开时间（此用户获取了帖子）'),
-    df.bigint('called_at').notNull().default(0).comment('最近被@时间'),
-    df.bigint('follow_at').notNull().default(0).comment('关注时间,0表示未关注'),
-    df.primary('post_id,user_id'),
-    df.foreign('post_id').references('post', 'id'),
-    df.foreign('user_id').references('user', 'id')
-]);
-
-// 回复帖子
-df.table('reply', [
-    df.unsigned('id').notNull().auto_increment(),
-    df.unsigned('post_id').notNull().comment('关联主题帖子ID'),
-    df.unsigned('reply_id').comment('所回复的回复帖子ID'),
-    df.unsigned('create_id').notNull().comment('发布用户ID'),
-    df.unsigned('edit_id').notNull().comment('最近修改人id'),
-    df.bigint('create_at').notNull().comment('创建时间'),
-    df.bigint('update_at').notNull().comment('最近修改时间'),
-    df.text('content').charset('utf8mb4').comment('回复内容'),
-    df.primary('id'),
-    df.index('post_id'),
-    df.foreign('create_id').references('user', 'id'),
-    df.foreign('edit_id').references('user', 'id'),
-    df.foreign('post_id').references('post', 'id')
 ]);
 
 df.table('qrcode', [
